fc8d980b9a14d086c93727c5fe7fae75f77057c7
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-08-09 14:49:37 +0900

    Solves Extract the domain name from a URL

---
+++ Python/5kyu/Extract-the-domain-name-from-a-URL.py
@@ -0,0 +1,14 @@
+# DESCRIPTION:
+# Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+# * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+# * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+# * url = "https://www.cnet.com"                -> domain name = cnet"
+
+# My Solution
+
+def domain_name(url):
+    import re
+    domain = re.sub("(https?://)?(www\.)?", "", url)
+    domain = re.sub("(\..*)", "", domain)
+    return domain
